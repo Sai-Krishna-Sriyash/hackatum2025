dcd1072739e161f663effcd1683fabc2103472f0
go76sit <Sriyashu@18-19>
2025-11-23 09:22:42 +0100

    .

--- frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  transpilePackages: ['react-map-gl', 'mapbox-gl'],
-  reactCompiler: true,
-};
-
-export default nextConfig;
